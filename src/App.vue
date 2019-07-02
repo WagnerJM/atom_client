83e690f7f0addb6a5d7d4ffd3cf2e554f25772b8
WagnerJM <45420760+WagnerJM@users.noreply.github.com>
2019-07-02 15:25:40 +0200

    project structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,10 @@
 <template>
-  <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld msg="Hello Vue in CodeSandbox!" />
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
+export default {};
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
